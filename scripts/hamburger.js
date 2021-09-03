bbad94f92b622b79b37077139fbd458834c3a2be
Alvin Ng <ngalvin2004@gmail.com>
2021-09-03 17:20:38 +0800

    Styled hamburger menu, added query selector for closing menu when nav button is clicked

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,9 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navContent = document.getElementById("mobile-nav");
 const body = document.querySelector("body");
-const mobileNavButtons = document.querySelector(".mobileNavButtons");
+const mobileNavButtons = document.querySelectorAll("#mobile-nav li a");
+
 hamburger.addEventListener("click", toggleMenu);
-mobileNavButtons.addEventListener("click", toggleMenu);
+
+for (let i = 0; i < mobileNavButtons.length; i++) {
+  mobileNavButtons[i].addEventListener("click", toggleMenu);
+}
 
 function toggleMenu(e) {
   hamburger.classList.toggle("open");
